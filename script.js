815d70c80adace8edd632c70536277b1183bd8cf
NebulaMakin <71604973+NebulaMakin@users.noreply.github.com>
2024-02-26 18:56:02 +0530

    Added some comment explaining the whole Increment Button thing.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
-const incrementbtn = document.querySelector('#increment')
+const incrementbtn = document.querySelector('#increment')               // This creates the instance of the Increment button  -> #1//
 const decrementbtn = document.querySelector('#decrement')
 let counter = 0
 
 function incrementCounter(){
-    const counterEl = document.getElementById('counter')
-    counter++
-    counterEl.innerText = counter
+    const counterEl = document.getElementById('counter')                //#2 The counterEl references the element with id = counter
+    counter++                                                           // editing the counter variable according to the function
+    counterEl.innerText = counter                                       // using the instance of the counter element edit it's value to be of counter//
+                                                                        // ex:- counter=0  -->  counter++  --> counter=1 and assigns the value to counter element
 }
 
 function decrementCounter(){
@@ -15,7 +16,7 @@ function decrementCounter(){
     counterEl.innerText = counter
 }
 
-incrementbtn.addEventListener('click', incrementCounter)
+incrementbtn.addEventListener('click', incrementCounter)                //#1 The same instance is used to call the incrementCounter() function  ->#2//
 
 
-decrementbtn.addEventListener('click', decrementCounter)
+decrementbtn.addEventListener('click', decrementCounter)                
